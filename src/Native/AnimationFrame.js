2bb1b5c674799f131dfee36961a6463c4d511027
Luke Westby <lwestby@alumni.nd.edu>
2016-05-04 07:33:57 -0500

    move call to Date.now() down

--- src/Native/AnimationFrame.js
+++ src/Native/AnimationFrame.js
@@ -4,11 +4,10 @@ var _elm_lang$animation_frame$Native_AnimationFrame = function()
 var rAF = _elm_lang$core$Native_Scheduler.nativeBinding(function(callback)
 {
 	var id = requestAnimationFrame(function(time) {
-		var now = Date.now();
 		var navStart = performance.timing.navigationStart;
 		var yieldedTime = time ?
 			(time > navStart ? time : time + navStart) :
-			now;
+			Date.now();
 		
 		callback(_elm_lang$core$Native_Scheduler.succeed(yieldedTime));
 	});
