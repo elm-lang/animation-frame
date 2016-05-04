fb95cd34316fd08bd50edf8fc170366091cb375c
Luke Westby <lwestby@alumni.nd.edu>
2016-05-04 07:53:22 -0500

    add safer lookup of navigationStart
    since Date.now() on script load is the closest possible approximation anyway and this way there's no need to leak out the requirement for a polyfill of performance.timing

--- src/Native/AnimationFrame.js
+++ src/Native/AnimationFrame.js
@@ -1,10 +1,13 @@
 var _elm_lang$animation_frame$Native_AnimationFrame = function()
 {
+	
+var navStart = window.performance && window.performance.timing && window.performance.timing.navigationStart ?
+	window.performance.timing.navigationStart :
+	Date.now();
 
 var rAF = _elm_lang$core$Native_Scheduler.nativeBinding(function(callback)
 {
 	var id = requestAnimationFrame(function(time) {
-		var navStart = performance.timing.navigationStart;
 		var yieldedTime = time ?
 			(time > navStart ? time : time + navStart) :
 			Date.now();
