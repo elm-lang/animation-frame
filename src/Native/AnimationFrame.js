437d4def020ead41224e5287fcd4adeecd840ee8
Luke Westby <lwestby@alumni.nd.edu>
2016-05-04 06:56:10 -0500

    account for quirks of timestamp passed to rAF callback

--- src/Native/AnimationFrame.js
+++ src/Native/AnimationFrame.js
@@ -4,7 +4,12 @@ var _elm_lang$animation_frame$Native_AnimationFrame = function()
 var rAF = _elm_lang$core$Native_Scheduler.nativeBinding(function(callback)
 {
 	var id = requestAnimationFrame(function(time) {
-		callback(_elm_lang$core$Native_Scheduler.succeed(time || Time.now()));
+		var now = Date.now();
+		var yieldedTime = time ?
+			(time < now ? (performance.timing.navigationStart + time) : time) :
+			now;
+		
+		callback(_elm_lang$core$Native_Scheduler.succeed(yieldedTime));
 	});
 
 	return function() {
