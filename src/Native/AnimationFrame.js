71b2d2bf27175983e5aaacb91ad15187ac165cdb
Evan Czaplicki <info@elm-lang.org>
2016-05-07 15:35:02 -0700

    Do some formatting and renaming

--- src/Native/AnimationFrame.js
+++ src/Native/AnimationFrame.js
@@ -1,18 +1,23 @@
 var _elm_lang$animation_frame$Native_AnimationFrame = function()
 {
-	
-var navStart = window.performance && window.performance.timing && window.performance.timing.navigationStart ?
-	window.performance.timing.navigationStart :
-	Date.now();
+
+var hasStartTime =
+	window.performance &&
+	window.performance.timing &&
+	window.performance.timing.navigationStart;
+
+var navStart = hasStartTime
+	? window.performance.timing.navigationStart
+	: Date.now();
 
 var rAF = _elm_lang$core$Native_Scheduler.nativeBinding(function(callback)
 {
 	var id = requestAnimationFrame(function(time) {
-		var yieldedTime = time ?
-			(time > navStart ? time : time + navStart) :
-			Date.now();
-		
-		callback(_elm_lang$core$Native_Scheduler.succeed(yieldedTime));
+		var timeNow = time
+			? (time > navStart ? time : time + navStart)
+			: Date.now();
+
+		callback(_elm_lang$core$Native_Scheduler.succeed(timeNow));
 	});
 
 	return function() {
