d7362a8c0061542b0d32b6ca0063f9dfe10bb255
Luke Westby <lwestby@alumni.nd.edu>
2016-08-24 17:36:36 -0500

    always use Date.now() as value of rAF task

--- src/Native/AnimationFrame.js
+++ src/Native/AnimationFrame.js
@@ -1,23 +1,9 @@
 var _elm_lang$animation_frame$Native_AnimationFrame = function()
 {
-
-var hasStartTime =
-	window.performance &&
-	window.performance.timing &&
-	window.performance.timing.navigationStart;
-
-var navStart = hasStartTime
-	? window.performance.timing.navigationStart
-	: Date.now();
-
 var rAF = _elm_lang$core$Native_Scheduler.nativeBinding(function(callback)
 {
-	var id = requestAnimationFrame(function(time) {
-		var timeNow = time
-			? (time > navStart ? time : time + navStart)
-			: Date.now();
-
-		callback(_elm_lang$core$Native_Scheduler.succeed(timeNow));
+	var id = requestAnimationFrame(function() {
+		callback(_elm_lang$core$Native_Scheduler.succeed(Date.now()));
 	});
 
 	return function() {
